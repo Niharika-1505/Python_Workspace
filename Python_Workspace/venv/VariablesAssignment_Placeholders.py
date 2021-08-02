d896b48745e1a03d2103a4153e859dfc2d7f347f
Niharika-1505 <gaddeniharika88@gmail.com>
2021-08-02 15:38:07 +0100

    Variables and Placeholders

--- Python_Workspace/venv/VariablesAssignment_Placeholders.py
+++ Python_Workspace/venv/VariablesAssignment_Placeholders.py
@@ -4,15 +4,14 @@ Sal = 30000
 # Assigning values to multiple variables in the same line
 Niha, Chandu, Nikki = 26, 24, 24
 # Assigning same value to multiple variables in the same line
-Developer = Tester = Devops = 50000
+Developer = Tester = Devops = 50000.00
 # Assigning both Integer and String values to multiple variables in the same line
 Salary, Job_Description = 350000, "Junior Software Developer"
 # Passing values as Tuples and printing them
-print("Age: %s \nSal: %s" % (Age, Sal))
-print("Ages of Niha, Chandu, Nikki are %s, %s, %s respectively" % (Niha, Chandu, Nikki))
-print("The salary of %s is %s" %(Job_Description, Salary))
+print("Age: %d \nSal: %d" % (Age, Sal))
+print("Ages of Niha, Chandu, Nikki are %d, %d, %d respectively" % (Niha, Chandu, Nikki))
+print("The salary of %s is %d" % (Job_Description, Salary))
 # Passing values as Dictionary and printing them
 print("Salaries of the jobs are as follows:\nDeveloper: %(a)s\nTester: %(b)s\nDevops: %(c)s" % {'a': Developer,
                                                                                                 'b': Tester,
                                                                                                 'c': Devops})
-
